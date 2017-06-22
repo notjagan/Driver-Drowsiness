b66d532aa1fb7f019a09088bb601c8ab959bc61f
Jagan Prem <notjagan@gmail.com>
2017-06-21 22:10:56 -0400

    Add code to calculate Eye Aspect Ratio (EAR)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import dlib
 import cv2
-import numpy
+import numpy as np
 from utils import Face
 from glob import glob
 from skimage import io
@@ -8,21 +8,27 @@ from skimage import io
 PRED_PATH = r"resources\shape_predictor_68_face_landmarks.dat"
 FILES = r"test_images\*.jpg"
 UPSCALE = 1
-ADJUST = 0
+ADJUST = -0.5
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(PRED_PATH)
-window = dlib.image_window()
 
 for file in glob(FILES):
     print("File: " + file)
     image = io.imread(file)
+    window = dlib.image_window()
     window.clear_overlay()
     window.set_image(image)
     try:
-        (detection, *_), scores, idx = detector.run(image, UPSCALE, ADJUST)
+        (detection, *_), (score, *_), idx = detector.run(image, UPSCALE, ADJUST)
     except ValueError:
         print("No faces found.")
+        window.wait_until_closed()
         continue
+    print("Face detected! (score " + str(score) + ")")
     landmarks = predictor(image, detection)
     window.add_overlay(landmarks)
+    face = Face(landmarks)
+    print("Left eye EAR:", face.left_eye.EAR())
+    print("Right eye EAR:", face.right_eye.EAR())
+    window.wait_until_closed()
--- utils.py
+++ utils.py
@@ -1,9 +1,22 @@
+import numpy as np
+from scipy.spatial.distance import euclidean as distance
+
 class Feature:
     def __init__(self, points):
-        pass
+        self.points = points
+
+class Eyebrow(Feature):
+    pass
 
 class Eye(Feature):
-    pass
+    def __init__(self, points):
+        super().__init__(points)
+        self.left_corner = points[0]
+        self.right_corner = points[3]
+    
+    def EAR(self):
+        p1, p2, p3, p4, p5, p6 = self.points
+        return (distance(p2, p6) + distance(p3, p5)) / (2 * distance(p1, p4))
 
 class Nose(Feature):
     pass
@@ -16,4 +29,19 @@ class Jawline(Feature):
 
 class Face:
     def __init__(self, landmarks):
-        pass
+        coords = np.zeros((landmarks.num_parts, 2))
+        for i, part in enumerate(landmarks.parts()):
+            coords[i] = (part.x, part.y)
+        self.jawline = Jawline(coords[:17])
+        self.left_eyebrow = Eyebrow(coords[17:22])
+        self.right_eyebrow = Eyebrow(coords[22:27])
+        self.nose = Nose(coords[27:36])
+        self.left_eye = Eye(coords[36:42])
+        self.right_eye = Eye(coords[42:48])
+        self.mouth = Mouth(coords[48:68])
+
+    def EAR(self, left):
+        if left:
+            return self.left_eye.EAR()
+        else:
+            return self.right_eye.EAR()
