98623e24c3acc6a26006bb1e62b74fe734a3b9d7
Jagan Prem <notjagan@gmail.com>
2017-06-22 20:45:32 -0400

    Add mouth aspect ratio and Perspective N-Point (PNP) calculations

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import dlib
 import cv2
 import numpy as np
-from utils import Face
+from utils import *
 from glob import glob
 from skimage import io
 
 PRED_PATH = r"resources\shape_predictor_68_face_landmarks.dat"
 FILES = r"test_images\*.jpg"
-UPSCALE = 1
+UPSCALE = 0
 ADJUST = -0.5
 
 detector = dlib.get_frontal_face_detector()
@@ -16,19 +16,46 @@ predictor = dlib.shape_predictor(PRED_PATH)
 for file in glob(FILES):
     print("File: " + file)
     image = io.imread(file)
-    window = dlib.image_window()
-    window.clear_overlay()
-    window.set_image(image)
+    img_cv = np.array(image[:, :, ::-1])
+    copy_cv = np.copy(img_cv)[:, :, :]
+    
     try:
         (detection, *_), (score, *_), idx = detector.run(image, UPSCALE, ADJUST)
     except ValueError:
         print("No faces found.")
-        window.wait_until_closed()
+        cv2.imshow("Image", img_cv)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
         continue
     print("Face detected! (score " + str(score) + ")")
+    
     landmarks = predictor(image, detection)
-    window.add_overlay(landmarks)
-    face = Face(landmarks)
-    print("Left eye EAR:", face.left_eye.EAR())
-    print("Right eye EAR:", face.right_eye.EAR())
-    window.wait_until_closed()
+    face = Face(img_cv, detection, landmarks)
+    
+    print("Left eye aspect ratio:", face.eye_aspect_ratio(left=True))
+    print("Right eye aspect ratio:", face.eye_aspect_ratio(right=True))
+    print("Mouth aspect ratio:", face.mouth_aspect_ratio())
+
+    for feature in face.features:
+        p = feature.points
+        for i, point in enumerate(p[:-1]):
+            cv2.line(copy_cv, tuple(np.round(point).astype(np.int)), tuple(np.round(p[i + 1]).astype(np.int)), (255, 0, 0), 1)
+
+    cv2.rectangle(copy_cv, *face.right_eye.bounding_box(), (0, 255, 0))    
+    cv2.rectangle(copy_cv, *face.left_eye.bounding_box(), (0, 255, 0))
+
+    print(face.right_eye.find_pupil())
+
+    for point in face.pnp_points:
+        cv2.circle(copy_cv, (int(point[0]), int(point[1])), 2, (0, 0, 255))
+    
+    rotation, translation = face.PnP()
+    print("Rotation vector:", tuple(rotation.reshape((1, 3))[0]))
+    axis = np.double([[0, 0, 1000]]).reshape(1, 3)
+    ((end_point, *_), *_), jacobian = cv2.projectPoints(axis, rotation, translation, *camera_internals(img_cv))
+    cv2.arrowedLine(copy_cv, tuple(np.round(face.nose.tip).astype(np.int)), tuple(np.round(end_point).astype(np.int)), (0, 0, 255), 1)
+
+    cv2.imshow("Facial Features", copy_cv)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    print()
--- utils.py
+++ utils.py
@@ -1,47 +1,119 @@
+import cv2
 import numpy as np
 from scipy.spatial.distance import euclidean as distance
 
+MODEL_COORDS = np.array((
+    (0, 0, 0),              # Tip of nose
+    (0, -330, -65),         # Chin
+    (-225, 170, -135),      # Left eye, left corner
+    (225, 170, -135),       # Right eye, right corner
+    (-150, -150, -125),     # Mouth, left corner
+    (150, -150, -125)       # Mouth, right corner
+), dtype=np.double)
+PNP_METHOD = cv2.SOLVEPNP_ITERATIVE
+
+def camera_internals(image):
+    size = image.shape
+    focal_length = size[1]
+    center = (size[1] // 2, size[0] // 2)
+    camera_matrix = np.array((
+        (focal_length, 0, center[0]),
+        (0, focal_length, center[1]),
+        (0, 0, 1)
+    ), dtype=np.double)
+    dist_coeffs = np.zeros((4, 1))
+    return camera_matrix, dist_coeffs
+
 class Feature:
-    def __init__(self, points):
+    def __init__(self, image, points):
         self.points = points
+        bbox = list(zip(*self.bounding_box()))
+        self.image = image[slice(*bbox[1]), slice(*bbox[0])]
+
+    def bounding_box(self):
+        return (tuple(np.round(self.points.min(axis=0)).astype(np.int)),
+                tuple(np.round(self.points.max(axis=0)).astype(np.int)))
 
 class Eyebrow(Feature):
     pass
 
 class Eye(Feature):
-    def __init__(self, points):
-        super().__init__(points)
+    def __init__(self, image, points):
+        super().__init__(image, points)
         self.left_corner = points[0]
         self.right_corner = points[3]
     
-    def EAR(self):
+    def aspect_ratio(self):
         p1, p2, p3, p4, p5, p6 = self.points
         return (distance(p2, p6) + distance(p3, p5)) / (2 * distance(p1, p4))
 
 class Nose(Feature):
-    pass
+    def __init__(self, image, points):
+        super().__init__(image, points)
+        self.tip = points[3]
 
 class Mouth(Feature):
-    pass
+    def __init__(self, image, points):
+        super().__init__(image, points)
+        self.left_corner = points[0]
+        self.right_corner = points[6]
+
+    def aspect_ratio(self):
+        p1, p2, p3, p4, p5, p6, p7, p8 = self.points[12:]
+        return (distance(p2, p8) + distance(p3, p7) + distance(p4, p6)) / (3 * distance(p1, p5))
 
 class Jawline(Feature):
-    pass
+    def __init__(self, image, points):
+        super().__init__(image, points)
+        self.chin = points[8]
 
 class Face:
-    def __init__(self, landmarks):
-        coords = np.zeros((landmarks.num_parts, 2))
+    def __init__(self, image, detection, landmarks):
+        self.original = image
+        self.image = image[detection.top():detection.bottom(), detection.left():detection.right()]
+        
+        coords = np.zeros((landmarks.num_parts, 2), dtype=np.double)
         for i, part in enumerate(landmarks.parts()):
             coords[i] = (part.x, part.y)
-        self.jawline = Jawline(coords[:17])
-        self.left_eyebrow = Eyebrow(coords[17:22])
-        self.right_eyebrow = Eyebrow(coords[22:27])
-        self.nose = Nose(coords[27:36])
-        self.left_eye = Eye(coords[36:42])
-        self.right_eye = Eye(coords[42:48])
-        self.mouth = Mouth(coords[48:68])
+            
+        self.features = [
+            self.jawline,
+            self.left_eyebrow,
+            self.right_eyebrow,
+            self.nose,
+            self.left_eye,
+            self.right_eye,
+            self.mouth
+        ] = [
+            Jawline(image, coords[:17]),
+            Eyebrow(image, coords[17:22]),
+            Eyebrow(image, coords[22:27]),
+            Nose(image, coords[27:36]),
+            Eye(image, coords[36:42]),
+            Eye(image, coords[42:48]),
+            Mouth(image, coords[48:68])
+        ]
+        
+        self.pnp_points = np.array((
+            self.nose.tip,
+            self.jawline.chin,
+            self.left_eye.left_corner,
+            self.right_eye.right_corner,
+            self.mouth.left_corner,
+            self.mouth.right_corner
+        ), dtype=np.double)
 
-    def EAR(self, left):
+    def eye_aspect_ratio(self, left=False, right=False):
         if left:
-            return self.left_eye.EAR()
-        else:
-            return self.right_eye.EAR()
+            if right:
+                return (self.left_eye.aspect_ratio() + self.right_eye.aspect_ratio()) / 2
+            return self.left_eye.aspect_ratio()
+        elif right:
+            return self.right_eye.aspect_ratio()
+
+    def mouth_aspect_ratio(self):
+        return self.mouth.aspect_ratio()
+
+    def PnP(self):
+        success, rotation, translation = cv2.solvePnP(MODEL_COORDS, self.pnp_points, *camera_internals(self.original), flags=PNP_METHOD)
+        return rotation, translation
